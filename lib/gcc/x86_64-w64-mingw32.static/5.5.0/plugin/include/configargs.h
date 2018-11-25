dc1f8f660700b0ed93bb98dbe82b3e5c62fc345c
drswinghead <drswinghead@gmail.com>
2018-11-25 21:05:03 +0800

    part2

---
+++ lib/gcc/x86_64-w64-mingw32.static/5.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/opt/mxe/tmp-gcc-x86_64-w64-mingw32.static/gcc-5.5.0/configure ac_cv_prog_HAVE_DOXYGEN=false --enable-doc=no --enable-gtk-doc=no --enable-gtk-doc-html=no --enable-gtk-doc-pdf=no --docdir=/opt/mxe/tmp-gcc-x86_64-w64-mingw32.static/gcc-5.5.0.build_.sink --infodir=/opt/mxe/tmp-gcc-x86_64-w64-mingw32.static/gcc-5.5.0.build_.sink --mandir=/opt/mxe/tmp-gcc-x86_64-w64-mingw32.static/gcc-5.5.0.build_.sink --with-html-dir=/opt/mxe/tmp-gcc-x86_64-w64-mingw32.static/gcc-5.5.0.build_.sink --disable-doxygen --target=x86_64-w64-mingw32.static --build=x86_64-pc-linux-gnu --prefix=/opt/mxe/usr --libdir=/opt/mxe/usr/lib --with-sysroot=/opt/mxe/usr/x86_64-w64-mingw32.static --enable-languages=c,c++,objc,fortran --enable-version-specific-runtime-libs --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --disable-shared --disable-multilib --without-x --disable-win32-registry --enable-threads=win32 --enable-libgomp --with-gmp=/opt/mxe/usr/x86_64-pc-linux-gnu --with-isl=/opt/mxe/usr/x86_64-pc-linux-gnu --with-mpc=/opt/mxe/usr/x86_64-pc-linux-gnu --with-mpfr=/opt/mxe/usr/x86_64-pc-linux-gnu --with-as=/opt/mxe/usr/bin/x86_64-w64-mingw32.static-as --with-ld=/opt/mxe/usr/bin/x86_64-w64-mingw32.static-ld --with-nm=/opt/mxe/usr/bin/x86_64-w64-mingw32.static-nm";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
